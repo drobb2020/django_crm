1ffd9e65a1cd9f46f21b6c028c961e82529deaa8
David Robb <66834033+drobb2020@users.noreply.github.com>
2021-09-06 10:55:28 -0400

    Leads app and database tables created

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'leads.apps.LeadsConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Toronto'
 
 USE_I18N = True
 
@@ -123,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'leads.User'
---
+++ leads/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Agent, Lead, User
+
+admin.site.register(Agent)
+admin.site.register(Lead)
+admin.site.register(User)
