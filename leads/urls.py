806b5defae1f66c206996514222813560ed28f3b
David Robb <66834033+drobb2020@users.noreply.github.com>
2021-09-06 15:21:31 -0400

    Leads CRUD functionality complete

--- leads/urls.py
+++ leads/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import lead_list, lead_detail, lead_create
+from .views import lead_list, lead_detail, lead_create, lead_update, lead_delete
 
 app_name = 'leads'
 
 urlpatterns = [
   path('', lead_list),
   path('create/', lead_create),
-  path('<int:pk>/', lead_detail)
+  path('<int:pk>/', lead_detail),
+  path('<int:pk>/update', lead_update),
+  path('<int:pk>/delete', lead_delete),
 ]
