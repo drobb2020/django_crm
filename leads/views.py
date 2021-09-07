5238420666a8f0db0d0fd7e80f497edf62d464f0
David Robb <66834033+drobb2020@users.noreply.github.com>
2021-09-06 20:02:18 -0400

    Base template and tailwind

--- leads/views.py
+++ leads/views.py
@@ -4,6 +4,10 @@ from .models import Agent, Lead, User
 from .forms import LeadForm, LeadModelForm
 
 
+def landing_page(request):
+  return render(request, 'landing.html')
+  
+
 def lead_list(request):
   leads = Lead.objects.all()
   context = {
