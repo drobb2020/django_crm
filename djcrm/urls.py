5f2890b254ad988ac54241b51f89882a3dfd6861
David Robb <66834033+drobb2020@users.noreply.github.com>
2021-09-07 16:11:25 -0400

    forgotten password system

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,7 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView, 
+    LogoutView, 
+    PasswordResetView, 
+    PasswordResetConfirmView, 
+    PasswordResetDoneView, 
+    PasswordResetCompleteView
+    )
 from django.urls import path, include
 from leads.views import LandingPageView, SignupView
 
@@ -11,6 +18,10 @@ urlpatterns = [
     path('agents/', include('agents.urls', namespace='agents')),
     path('leads/', include('leads.urls', namespace='leads')),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
